c646f8dfb66a77672ec07ecad61fd5c5ca97211a
giovani <giovanibossoni@hotmail.com>
2018-09-12 17:23:03 -0300

    Iniciando A implementação do feed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FeedComponent } from './feed/feed.component';
+import { FeedItemComponent } from './feed-item/feed-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedComponent,
+    FeedItemComponent
   ],
   imports: [
     BrowserModule
