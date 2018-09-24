14a27943c6f135a79d3c18cca61118c83402f175
Giovani <giovanibossoni@hotmail.com>
2018-09-24 14:07:23 -0300

    Adicionando uma barra de navegação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FeedComponent } from './feed/feed.component';
 import { FeedItemComponent } from './feed-item/feed-item.component';
+import { BarraNavegacaoComponent } from './barra-navegacao/barra-navegacao.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FeedComponent,
-    FeedItemComponent
+    FeedItemComponent,
+    BarraNavegacaoComponent
   ],
   imports: [
     BrowserModule
