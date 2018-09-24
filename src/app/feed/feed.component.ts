fa39b0debf92cadaa0e36aaf4fc084d4307c553d
Giovani <giovanibossoni@hotmail.com>
2018-09-24 10:50:00 -0300

    Feed com *ngFor

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedComponent implements OnInit {
 
+  // lista do feed
+  lista = ["1","2","3"];
   constructor() { }
 
   ngOnInit() {
