f9b83f78a5610305943e9a838b62a6d80978c7bb
GIOVANI BOSSONI <gbossoni@AD.MPPR.MP.BR>
2018-11-30 11:33:24 -0200

    Adicionado mouse hover na barra lateral

--- src/app/barras/barra-lateral/barra-lateral.component.ts
+++ src/app/barras/barra-lateral/barra-lateral.component.ts
@@ -24,11 +24,12 @@ export class BarraLateralComponent implements OnInit {
   }
   openNav() {
     document.getElementById('mySidenav').style.width = '250px';
+    
   }
 
   /* Set the width of the side navigation to 0 */
   closeNav() {
-    document.getElementById('mySidenav').style.width = '0';
+    document.getElementById('mySidenav').style.width = '1px';
   }
 
 }
