4ff9f34d1d7842e9abaadf636c1b628cf0009efa
José Moscardi da Silva Junior <jmoscajr@gmail.com>
2018-12-01 14:20:09 -0200

    página de listagem das reservas do usuário pronta

---
+++ src/app/model/reservaUsuario.model.ts
@@ -0,0 +1,13 @@
+
+// tslint:disable-next-line:class-name
+export class reservaUsuario {
+  idReserva: number;
+  data: Date;
+  idPromocao: number;
+  descricaoPromocao: string;
+  nomePrato: string;
+  caminhoImagem: string;
+  descricaoPrato: string;
+  idRestaurante: number;
+  nomeRestaurante: string;
+}
--- src/app/services/reserva.service.ts
+++ src/app/services/reserva.service.ts
@@ -1,9 +1,41 @@
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { reservaUsuario } from '../model/reservaUsuario.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservaService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  retornarTodasReservas(idusuario: number, callback): void {
+    const reservas: reservaUsuario[] = [];
+    this.http
+      .get(
+        'http://localhost:3000/reserva//buscarReservasPorUsuario/' + idusuario
+      )
+      .subscribe(retorno => {
+        for (const row of retorno['dados']) {
+          const aux: reservaUsuario = this.converteReservaBack(row);
+          console.log(aux);
+          reservas.push(aux);
+        }
+      });
+
+    callback(reservas);
+  }
+
+  private converteReservaBack(dado) {
+    const aux: reservaUsuario = new reservaUsuario();
+    aux.idReserva = dado['idreserva'];
+    aux.data = new Date(dado['data']);
+    aux.idPromocao = dado['dipromocao'];
+    aux.nomePrato = dado['nomePrato'];
+    aux.caminhoImagem = dado['idimagem'];
+    aux.descricaoPrato = dado['descricao'];
+    aux.nomeRestaurante = dado['nomeRestaurante'];
+    aux.descricaoPromocao = undefined;
+    return aux;
+  }
 }
